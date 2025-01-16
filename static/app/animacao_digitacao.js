b59895be30b0696af9d33173c373e58b1c953ff8
Fabiovieira05 <fabioalessandro1010@gmail.com>
2025-01-16 12:58:09 -0300

    Organizei um pouco os arquivos e fiz um pouco da responsividade

---
+++ static/app/animacao_digitacao.js
@@ -0,0 +1,23 @@
+var titulo = document.getElementById('titulo_principal');
+var texto_titulo = titulo.innerHTML;
+var i = 0
+
+const escrever = () => {
+  titulo.innerHTML = titulo.innerHTML.replace('|', '')
+
+  if (texto_titulo.length > i) {
+    if (i == 0){
+      titulo.innerHTML = texto_titulo.charAt(i) 
+    } else {
+      titulo.innerHTML += texto_titulo.charAt(i) 
+    }
+
+    titulo.innerHTML += '|'
+
+    i++
+    setTimeout(escrever, 80)
+
+  } 
+}
+
+escrever()
